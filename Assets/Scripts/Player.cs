7750f2dcdc71671b3b34915ff369c60225dcebc1
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-10-14 11:50:00 +0700

    add: feature move player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,30 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5;
     PlayerController controller;
 
+    Camera viewCamera;
+
     void Start()
     {
         controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
     }
 
     void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
+
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if( groundPlane.Raycast(ray, out rayDistance)){
+            Vector3 point = ray.GetPoint(rayDistance);
+            // Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,12 @@ public class PlayerController : MonoBehaviour
         myRigidBody = GetComponent<Rigidbody >();
     }
 
+    public void LookAt(Vector3 lookPoint){
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x,transform.position.y, lookPoint.z);
+        // transform.LookAt(lookPoint);
+        transform.LookAt(heightCorrectedPoint);
+
+    }
     public void Move(Vector3 _velocity){
         velocity = _velocity;
     }
