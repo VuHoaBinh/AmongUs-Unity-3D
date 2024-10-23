9f2ccb6adc26353c3d4fa2e7febcf3242a9b8926
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-10-23 14:52:16 +0700

    add sprint 1: fix feature Gun

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public Transform muzzle;
+    public enum FireMode{Auto, Burst, Single};
+    public FireMode fireMode;
+    public Transform[] projecttileSpawn;
     public ProjectTitle projectTitle;
     public float msBetweenShots = 100;
     public float muzzleVelocity = 35;
+    public int burstCount;
+    public Transform shell;
+    public Transform shellEjection;
+    MuzzleFlash muzzleFlash;
     float nextShotTime;
 
-    public Transform shell;
-
-    public Transform shellEjection;
-
-    MuzzleFlash muzzleFlash;
+    bool triggerReleaseSinceLastShot;
+    int shotsRemainingInBurst;
 
     void Start(){
         muzzleFlash = GetComponent<MuzzleFlash>();
+        shotsRemainingInBurst  = burstCount;
     }
-    public void Shoot()
+    void Shoot()
     {
         if(Time.time > nextShotTime){
-            nextShotTime = Time.time + msBetweenShots / 1000;
-            ProjectTitle newProjectile = Instantiate(projectTitle, muzzle.position, muzzle.rotation) as ProjectTitle;
-            newProjectile.SetSpeed(muzzleVelocity);
-            // Destroy(newProjectile, 5f);
+            if (fireMode == FireMode.Burst){
+                if(shotsRemainingInBurst == 0){
+                    return;
+                }
+                shotsRemainingInBurst--;
+            }else if(fireMode == FireMode.Single){
+                 if(!triggerReleaseSinceLastShot){
+                    return;
+                }
+            }
 
+            for (int i = 0; i < projecttileSpawn.Length; i++)
+            {
+                nextShotTime = Time.time + msBetweenShots / 1000;
+                ProjectTitle newProjectile = Instantiate(projectTitle, projecttileSpawn[i].position, projecttileSpawn[i].rotation) as ProjectTitle;
+                newProjectile.SetSpeed(muzzleVelocity);
+                // Destroy(newProjectile, 5f);
+
+                
+            }
             Instantiate(shell, shellEjection.position, shellEjection.rotation);
             muzzleFlash.Activate();
         }
+    }
+
+    public void OnTriggerHold(){
+        Shoot();
+        triggerReleaseSinceLastShot = false;
+    }
+
+    public void OnTriggerRelease(){
+        triggerReleaseSinceLastShot = true;
+        shotsRemainingInBurst  = burstCount;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -37,7 +37,11 @@ public class Player : LivingEntity {
 
 		// Weapon input
 		if (Input.GetMouseButton(0)) {
-			gunController.Shoot();
+			gunController.OnTriggerHold();
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			gunController.OnTriggerRelease();
 		}
 	}
 }
