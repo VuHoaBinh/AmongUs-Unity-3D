3696d80df2c526e1346857ad980cb30beb0a671b
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-11-27 00:19:01 +0700

    animation: anounce

---
+++ Assets/Scripts/PopupWindow.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PopupWindow : MonoBehaviour
+{
+    public TMP_Text  popupText;
+
+    private GameObject window;
+    private Animator popupAnimator;
+
+    private Queue<string> popupQueue; 
+    private Coroutine queueChecker;
+
+    private void Start() {
+        window = transform.GetChild(0).gameObject;
+        popupAnimator = window.GetComponent<Animator>();
+        window.SetActive(false);
+        popupQueue = new Queue<string>();
+    }
+
+    public void AddToQueue(string text) {
+        popupQueue.Enqueue(text);
+        if (queueChecker == null)
+            queueChecker = StartCoroutine(CheckQueue());
+    }
+
+    private void ShowPopup(string text) {
+        window.SetActive(true);
+        popupText.text = text;
+        popupAnimator.Play("PopupAnimation");
+    }
+
+    private IEnumerator CheckQueue() {
+        do {
+            ShowPopup(popupQueue.Dequeue());
+            do {
+                yield return null;
+            } while (!popupAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Idle"));
+
+        } while (popupQueue.Count > 0);
+        window.SetActive(false);
+        queueChecker = null;
+    }
+}
